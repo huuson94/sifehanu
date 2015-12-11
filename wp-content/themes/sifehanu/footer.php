88db1758b9bb9083d16e8da91a7f5ce0eef9fc48
daonhat <daonhat123@gmail.com>
2015-12-12 01:33:23 +0700

    base html

--- wp-content/themes/sifehanu/footer.php
+++ wp-content/themes/sifehanu/footer.php
@@ -1,20 +1,3 @@
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/plugins/contact-form-7/includes/js/jquery.form.min.js?ver=3.51.0-2014.06.20'></script>
-<script type='text/javascript'>
-/* <![CDATA[ */
-var _wpcf7 = {"loaderUrl":"http:\/\/tec-ftu.org\/wp-content\/plugins\/contact-form-7\/images\/ajax-loader.gif","recaptchaEmpty":"Please verify that you are not a robot.","sending":"Sending ..."};
-/* ]]> */
-</script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/plugins/contact-form-7/includes/js/scripts.js?ver=4.3.1'></script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/themes/simplemag/js/jquery.flexslider-min.js?ver=2.1'></script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/themes/simplemag/js/jquery.caroufredsel.js?ver=6.2.0'></script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/themes/simplemag/js/jquery.touchSwipe.min.js?ver=1.3.3'></script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/themes/simplemag/js/jquery.assets.js?ver=1.0'></script>
-<script type='text/javascript' src='http://tec-ftu.org/wp-content/themes/simplemag/js/jquery.custom.js?ver=1.0'></script>
-<script type='text/javascript'>
-/* <![CDATA[ */
-var FB_WP=FB_WP||{};FB_WP.queue={_methods:[],flushed:false,add:function(fn){FB_WP.queue.flushed?fn():FB_WP.queue._methods.push(fn)},flush:function(){for(var fn;fn=FB_WP.queue._methods.shift();){fn()}FB_WP.queue.flushed=true}};window.fbAsyncInit=function(){FB.init({"xfbml":true,"appId":"238532092960323"});if(FB_WP && FB_WP.queue && FB_WP.queue.flush){FB_WP.queue.flush()}}
-/* ]]> */
-</script>
-<script type="text/javascript">(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id)){return}js=d.createElement(s);js.id=id;js.src="http:\/\/connect.facebook.net\/en_US\/all.js";fjs.parentNode.insertBefore(js,fjs)}(document,"script","facebook-jssdk"));</script>
-<div id="fb-root"></div></body>
+
+</body>
 </html>
